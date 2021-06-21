a8b102289a4d63e57b296dd437e5f6675784db38
Gabriel7H <gabrielamorim338@outlook.com>
2021-06-21 16:25:11 -0300

    new version

--- newbootstrap/src/index.js
+++ newbootstrap/src/index.js
@@ -1,9 +1,12 @@
 import * as bootstrap from 'bootstrap';
+
 const logo = document.querySelectorAll('#logo path');
 
 for (let i = 0; i<logo.length; i++){
     console.log(`Letter ${i} is ${logo[i].getTotalLength()}`);
 }
+
+
 
 
 
